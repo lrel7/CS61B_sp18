aa4a800550ddf379abd0797afb53fb7c2d116f7f
xyl <lrel7@mail.ustc.edu.cn>
2023-08-01 11:25:58 +0800

    fix ad

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,100 +1,124 @@
-public class ArrayDeque<T>{
+public class ArrayDeque<T> {
     private T[] arr; // array to save data
     private int arr_len; // length of the arr
     private int size; // number of items in the deque
     private int front; // index of the front
     private int last; // index of the last
 
-    public ArrayDeque(){
-        arr = (T[])new Object[8]; // init an array of length 8
+    public ArrayDeque() {
+        arr = (T[]) new Object[8]; // init an array of length 8
         arr_len = 8;
         size = 0;
         front = 0;
         last = 0;
     }
 
-    private void grow(int capacity){
-        T[] new_arr = (T[])new Object[capacity];
+    private void grow(int capacity) {
+        T[] new_arr = (T[]) new Object[capacity];
         System.arraycopy(arr, 0, new_arr, 0, arr_len);
         arr = new_arr;
         arr_len = capacity;
     }
 
-    private int move_forward(int index){
-        int mod = (index - 1) % arr_len;
-        return mod < 0 ? mod + arr_len : mod;
+    private void shrink() {
+        T[] new_arr = (T[]) new Object[arr_len / 2];
+        int ptr = front;
+        for(int i = 0; i < size; i++) {
+            new_arr[i] = arr[ptr];
+            ptr = move_backward(ptr);
+        }
+        arr = new_arr;
+        front = 0;
+        last = size - 1;
+        arr_len /= 2;
     }
 
-    private int move_backward(int index){
-        return (index + 1) % arr_len;
+    private int move_forward(int index) {
+        if (index == 0) {
+            return arr_len - 1;
+        }
+        return index - 1;
     }
 
-    public void addFirst(T item){
-        if(arr_len == size){
+    private int move_backward(int index) {
+        if(index == arr_len - 1) {
+            return 0;
+        }
+        return index + 1;
+    }
+
+    public void addFirst(T item) {
+        if (arr_len == size) {
             grow(arr_len * 3);
-        }        
+        }
         front = move_forward(front);
         arr[front] = item;
-        size ++;
+        size++;
     }
 
-    public void addLast(T item){
-        if(arr_len == size){
+    public void addLast(T item) {
+        if (arr_len == size) {
             grow(arr_len * 3);
-        }        
+        }
         last = move_backward(last);
         arr[last] = item;
-        size ++;
+        size++;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
+    public void printDeque() {
         int i = front;
-        while(true){
-            System.out.println(arr[i] + " ");
-            if(i == last){
+        while (true) {
+            System.out.print(arr[i] + " ");
+            if (i == last) {
                 break;
             }
             i = move_backward(i);
         }
     }
 
-    public T removeFirst(){
-        if(isEmpty()){
+    public T removeFirst() {
+        if (isEmpty()) {
             return null;
         }
         T ret = arr[front];
         front = move_backward(front);
-        size --;
+        size--;
+        if(arr_len >= 16 && arr_len / size >= 4) {
+            shrink();
+        }
         return ret;
     }
 
-    public T removeLast(){
-        if(isEmpty()){
+    public T removeLast() {
+        if (isEmpty()) {
             return null;
         }
         T ret = arr[last];
         last = move_forward(last);
-        size --;
+        size--;
+        if(arr_len >= 16 && arr_len / size >= 4) {
+            shrink();
+        }
         return ret;
     }
-    
-    public T get(int index){
-        if(size <= index){
+
+    public T get(int index) {
+        if (size <= index) {
             return null;
         }
         int pos = front;
-        while(index > 0){
+        while (index > 0) {
             pos = move_backward(pos);
-            index --;
+            index--;
         }
         return arr[pos];
-    }    
+    }
 }
